4abe9002516ba750527476a1333c40ce4f36b0e3
Niema-git <elachaouiniema@gmail.com>
2026-05-11 09:17:32 +0100

    Initial commit

---
+++ main/java/org/sid/contratassurance/ContratAssuranceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.contratassurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratAssuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContratAssuranceApplication.class, args);
+    }
+
+}
---
+++ test/java/org/sid/contratassurance/ContratAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.contratassurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratAssuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
